f62af9ef2c35f5a67613d71d79e7db83ac5411e1
Andrew Taylor <drew.j.taylor1@gmail.com>
2022-12-17 20:56:10 -0500

    Localstorage saving list

--- src/App.js
+++ src/App.js
@@ -5,18 +5,16 @@ import './App.css';
 
 function App() {
     // To work on displaying list properly, set up a "dummy" list state, including the name of the user and some list items
-    let testState = JSON.parse(localStorage.getItem('todos'));
+
     const [listState, SetListState] = useState(() => {
         const savedState = localStorage.getItem('todoState');
+        console.log(savedState);
 
-        if (savedState) {
-            return JSON.parse(savedState)
-        } else {
-            return dummyData
-        }
-    })
+        return JSON.parse(localStorage.getItem('todoState')) || dummyData
+        })
 
 
+    // Every time the "listState" is updated, update local storage keyto match
     useEffect(() => {
         localStorage.setItem('todoState', JSON.stringify(listState));
     }, [listState]);
@@ -34,7 +32,7 @@ function App() {
             }
         })
 
-        SetListState(updatedState)
+        SetListState(updatedState);
     }
 
   return (
@@ -47,6 +45,10 @@ function App() {
                 <hr />
             </div>
         }))}
+
+        <button onClick={() => {
+            console.log(JSON.stringify(localStorage.getItem(('todoState')), null, 2))
+        }}>Press button to check "localstorage.todoState"</button>
     </>
   );
 }
