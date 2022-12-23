303632c68683415eb2d970ec18a1759eda86efd4
Andrew Taylor <drew.j.taylor1@gmail.com>
2022-12-22 21:51:01 -0500

    Simplified ListItem conditional display

--- src/ListItem.js
+++ src/ListItem.js
@@ -1,24 +1,13 @@
 const ListItem = ({item}) => {
     const {title, description, completed} = item;
-  
-  
-    if (!completed) {
-        return (
-            <>
-                <h3 className="item-title">{title}</h3>
-                <h5 className="item-description">{description}</h5>
-            </>
-      )
-    } else {
-        return (
-            <>
-            <h3 className="item-title completed">{title}</h3>
-            <h5 className="item-description completed">{description}</h5>
-            </>
-      )
 
-    }
-  
+
+        return (
+            <>
+                <h3 className={completed ? "item-title" : "item-title completed"}>{title}</h3>
+                <h5 className={completed ? "item-description" : "item-description completed"}>{description}</h5>
+            </>
+        )
 }
 
 export default ListItem
