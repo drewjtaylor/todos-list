f3d4ba75ac34e61a87cca71c214940a975bf2098
Andrew Taylor <drew.j.taylor1@gmail.com>
2022-12-24 22:49:09 -0500

    More visual tweaking

--- src/ListItem.js
+++ src/ListItem.js
@@ -5,7 +5,7 @@ const ListItem = ({item}) => {
         return (
             <>
                 <h3 className={!completed ? "item-title" : "item-title completed"}>{title}</h3>
-                <h5 className={!completed ? "item-description" : "item-description completed"}>{description}</h5>
+                <p className={!completed ? "item-description" : "item-description completed"}>{description}</p>
             </>
         )
 }
